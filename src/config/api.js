76ab540722c1f48be0c2eff1671b57eca9475c79
Dicky Joel Saputra <dickyjoelsaputra@gmail.com>
2022-12-12 08:12:33 +0700

    first commit

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-    baseURL: process.env.REACT_APP_BASEURL
+    baseURL: process.env.REACT_APP_BASEURL,
     // Get REACT_APP_BASEURL from env here ...
     // baseURL: "http://localhost:5000/api/v1/",
 });
